c4cd6d835e4e0214159b747cdee48666e341056b
Erick Flore <ferick8246@gmail.com>
2021-05-13 22:31:15 -0500

    SERVER

--- server.js
+++ server.js
@@ -3,4 +3,46 @@ const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> {})
+
+app.use (express.urlencoded({extended: true}));
+app.use (express.json());
+app.use (express.static(path.join(__dirname, "public")));
+// HTML 
+app.get("*", (req, res ) =>  
+{res.sendFile(path.join(__dirname, "index.html"))
+});
+
+app.get("/notes", (req, res ) =>  
+{res.sendFile(path.join(__dirname, "notes.html"))
+});
+
+app.get("/api/notes", (req, res)=>(
+    res.json()
+))
+
+app.post("/api/notes", (req, res)=>(
+    res.json()
+))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=> console.log(`The Note Taker is Listing on PORT ${PORT}`));
